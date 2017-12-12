8615ad074e66ab932e1b530b1032e689a41e6d36
刘城熙 <1013688053@qq.com>
2017-12-12 21:09:47 +0800

    1

---
+++ WST.Web.Framework/Filters/ShopLoginFilterAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WST.IService;
+using WST.Core.Model;
+using WST.Core.Code;
+using WST.Service;
+using WST.Core.Util;
+using WST.Core.Extensions;
+
+namespace WST.Web.Framework.Filters
+{
+    /// <summary>
+    /// 过滤器
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ShopLoginFilterAttribute : ActionFilterAttribute
+    {
+        public ShopLoginFilterAttribute()
+        { }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var controller = filterContext.Controller as BaseUserController;
+
+            if (!controller.LoginUser.IsMember)
+            {
+                RedirectResult redirectResult = new RedirectResult("/user/index");
+                filterContext.Result = redirectResult;
+            }
+
+            if (controller.LoginUser.EndTime<DateTime.Now)
+            {
+                RedirectResult redirectResult = new RedirectResult("/user/buytime");
+                filterContext.Result = redirectResult;
+            }
+
+            
+        }
+    }
+}
